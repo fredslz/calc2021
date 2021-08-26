5cba9a2262708480461cbb1c8eaa77d365d51eff
fredslz <fredslz@gmail.com>
2021-08-25 21:19:57 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -1,7 +1,7 @@
 import pytest
 from meuapp import soma
 
-@pytest.mark.parametrize('num1,num2,valoresperado', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ('DOIS', 2, 4), (2,,2])
+@pytest.mark.parametrize('num1,num2,valoresperado', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ('DOIS', 2, 4), (2,,2)])
 def test_soma(num1,num2, valoresperado):
     assert soma(num1,num2) == valoresperado
 
