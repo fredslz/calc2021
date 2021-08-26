78a04d6be70594c25ac583f6f5ad50ac025f5f4d
fredslz <fredslz@gmail.com>
2021-08-25 21:23:11 -0300

    Update meuapp.py

--- meuapp.py
+++ meuapp.py
@@ -1,3 +1,14 @@
 
 def soma(num1, num2):
-    return num1 + num2
+    
+    if is_number(num1) and is_number(num2):
+        return float(a) + float(b)
+    else:
+        return None
+    
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
